99a926fd0986f83e01859c7f67b7d62301ff8fe2
Dmitrii <deeemmmmka777@yandex.ru>
2021-02-19 16:27:12 +0300

    first time

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello");
+
+let answer = prompt("Есть ли вам 18?", "Да");
+
+console.log(typeof(answer));
+
