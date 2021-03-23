602bd11cadba9a5e4991c2ff98ab0226c3e24e95
Dmitrii <deeemmmmka777@yandex.ru>
2021-03-23 14:33:04 +0300

    new script

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 'use strict';
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false
-}
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: {},
+    savings: false
+};
 
-console.log(arr[0]);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
 
-// alert("Hello");
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-let answer = prompt("Есть ли вам 18?", "Да");
-
-console.log(typeof(answer));
+alert(appData.budget / 30);
 
